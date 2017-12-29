886f06682f6b0a8d314ee89284c1e8fd07c4be44
Tenjiuin Saki <498172524@qq.com>
2017-12-29 14:24:31 +0800

    修改名称

--- TSExtension/ViewController.swift
+++ TSExtension/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        let btn = UIButton()
+        btn.title = "1"
     }
 
     override func didReceiveMemoryWarning() {
---
+++ TSExtensionFramework/TSExtensionFramework.h
@@ -0,0 +1,19 @@
+//
+//  TSExtensionFramework.h
+//  TSExtensionFramework
+//
+//  Created by mc on 2017/12/29.
+//  Copyright © 2017年 张玉飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TSExtensionFramework.
+FOUNDATION_EXPORT double TSExtensionFrameworkVersionNumber;
+
+//! Project version string for TSExtensionFramework.
+FOUNDATION_EXPORT const unsigned char TSExtensionFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TSExtensionFramework/PublicHeader.h>
+
+
